4a18af679ab069ebbcdae12535daa3d9211e23e1
Veles Voyage Deploy <deploy@veles-voyage.ru>
2026-07-29 16:45:22 +0300

    fix: remove duplicate infoflot widget from cruises layout

--- src/app/cruises/layout.tsx
+++ src/app/cruises/layout.tsx
@@ -1,5 +1,4 @@
 import { Breadcrumbs } from '@/shared/components/ui/Breadcrumbs';
-import Script from 'next/script';
 
 export default function CruisesLayout({
     children,
@@ -12,16 +11,6 @@ export default function CruisesLayout({
                 <Breadcrumbs />
             </div>
             {children}
-            <div
-                className="infoflotWidget"
-                data-id="YTo0OntzOjI6IklEIjtzOjQ6IjQxMjkiO3M6NDoiVVNFUiI7czozMjoiY3k1emRtbHpkSFZ1YjNaQWFHOTBiV0ZwYkM1amIyMD0iO3M6NjoiUkFORE9NIjtzOjg6InRkb2ZhODA3IjtzOjE1OiJJTkZPRkxPVC1BUElLRVkiO3M6NDA6ImRiYzY0NDU0Mjc1ODQxMGNhMjg0MTgwNmI5ZTQ5YzdlZTY3NjY5ZTAiO30="
-                data-index="1"
-            />
-            <Script
-                src="https://bitrix.infoflot.com/local/templates/infoflot/frontend/js/infoflotIframe.js"
-                strategy="afterInteractive"
-                async
-            />
         </>
     );
 }
